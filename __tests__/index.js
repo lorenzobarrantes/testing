9bedd101e0e0832166f1e629be46c4c9d8a6842c
Lorenzo Barrantes <lorenzob@mellon.com.ar>
2023-04-17 14:09:59 -0300

    login ok

---
+++ __tests__/index.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+
+describe('Inicio de sesión', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    // Iniciar el navegador y abrir una página nueva
+    browser = await puppeteer.launch({ 
+      headless: false
+    });
+    page = await browser.newPage();
+  });
+
+  // afterAll(async () => {
+  //   // Cerrar el navegador al terminar todas las pruebas
+  //   await browser.close();
+  // });
+
+  test('El usuario puede iniciar sesión', async () => {
+    // Ir a la página de inicio de sesión
+    
+    await page.goto('http://emorell-dt.dnsalias.com:23060/SODA_PREPROD_CONDE/login.zul#/warehouses');
+    page.waitForNavigation(); // Esperar a que la página cargue completamente
+
+    // Completar los campos de usuario y contraseña
+    await page.type('#u', 'lbarrantes');
+    await page.type('#p', '123456');
+
+    // Hacer clic en el botón de inicio de sesión
+
+    await page.waitForSelector('.z-button');
+    await page.click('.z-button');
+  
+    // Verificar que el usuario ha iniciado sesión exitosamente
+  });
+
+
+  test('Operaciones', async () => {
+    // Ir a la página de inicio de sesión
+    
+    await page.goto('http://emorell-dt.dnsalias.com:23060/SODA_PREPROD_CONDE/#/operations/inputs/orders');
+    page.waitForNavigation(); // Esperar a que la página cargue completamente
+
+    // // Completar los campos de usuario y contraseña
+    // await page.type('#u', 'lbarrantes');
+    // await page.type('#p', '123456');
+
+    // // Hacer clic en el botón de inicio de sesión
+
+    // await page.waitForSelector('.z-button');
+    // await page.click('.z-button');
+
+    // // Verificar que el usuario ha iniciado sesión exitosamente
+  });
+  });
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testMatch: [
+      "**/__tests__/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[tj]s?(x)"
+    ],
+    testPathIgnorePatterns: [
+      "/node_modules/"
+    ],
+    // testTimeout: 100000,
+    detectOpenHandles: true
+  };
