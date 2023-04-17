b6e9c0621341378b831a9e47a236d96d11f26b89
lorenzobarrantes <lorenzob@mellon.com.ar>
2023-04-17 16:25:56 -0300

    para editar

--- __tests__/index.js
+++ __tests__/index.js
@@ -39,18 +39,15 @@ describe('Inicio de sesión', () => {
   test('Operaciones', async () => {
     // Ir a la página de inicio de sesión
     
-    await page.goto('http://emorell-dt.dnsalias.com:23060/SODA_PREPROD_CONDE/#/operations/inputs/orders');
     page.waitForNavigation(); // Esperar a que la página cargue completamente
+    await page.waitForSelector('Operaciones');
+    await page.click('Operaciones');
+    await page.waitForSelector('Arribos y p');
+    await page.click('arribosyp');
+    await page.waitForSelector('TomarArribos');
+    await page.click('tomararribos');
+    await page.waitForSelector('arriboNoPlanificado');
+    await page.click('arriboNo');
 
-    // // Completar los campos de usuario y contraseña
-    // await page.type('#u', 'lbarrantes');
-    // await page.type('#p', '123456');
-
-    // // Hacer clic en el botón de inicio de sesión
-
-    // await page.waitForSelector('.z-button');
-    // await page.click('.z-button');
-
-    // // Verificar que el usuario ha iniciado sesión exitosamente
   });
   });
